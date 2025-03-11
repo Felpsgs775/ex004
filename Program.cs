4b8404a4621619d194c3ea1feadc0265c29eb2c2
Felipe Garcia Silva A.K.A. Felps <139228026+Felpsgs775@users.noreply.github.com>
2025-03-11 12:12:25 -0300

    Exercicio 3

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace exercicio3
+{
+    class Program
+    {
+     static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int D = int.Parse(Console.ReadLine());
+
+            int produto1 = A * B;
+            int produto2 = C * D;
+            int diferenca = produto1 - produto2;
+
+            Console.WriteLine(diferenca);
+
+
+
+
+
+        }  
+    }
+}
